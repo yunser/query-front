9cc4ab9480ccbf70b0cdefa959b7593ea84dabf8
cjh <1418503647@qq.com>
2018-02-10 21:51:42 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <my-page title="首页">
+        这是首页内容
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
