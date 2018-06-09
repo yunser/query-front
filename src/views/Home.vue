ca17a889a411f9d4b7d4d5a0d46b85f46caccaba
cjh <1418503647@qq.com>
2018-06-09 18:49:43 +0800

    support data param

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,9 @@
         <ui-text-field label="手机号码" v-model="phone" />
         <br>
         <ui-raised-button class="btn" label="查询" primary @click="query" />
+        <div class="ui-loading" v-if="loading">
+            <ui-circular-progress :size="24"/>
+        </div>
         <ui-article v-if="result">
             <table>
                 <tr>
@@ -22,8 +25,16 @@
     export default {
         data () {
             return {
+                loading: false,
                 phone: '',
                 result: null
+            }
+        },
+        mounted() {
+            let data = this.$route.query.data
+            if (data) {
+                this.phone = data
+                this.query()
             }
         },
         methods: {
@@ -35,13 +46,17 @@
                     })
                     return
                 }
+                this.loading = true
+                this.result = null
                 this.$http.get('/phone?phone=' + this.phone).then(
                     response => {
                         let data = response.data
                         console.log(data)
+                        this.loading = false
                         this.result = data
                     },
                     response => {
+                        this.loading = false
                         console.log(response)
                     })
             }
@@ -52,5 +67,14 @@
 <style scoped>
     .btn {
         margin-bottom: 16px;
+    }
+    .ui-loading {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        box-shadow: 0 1px 6px rgba(0,0,0,.117647), 0 1px 4px rgba(0,0,0,.117647);
     }
 </style>
